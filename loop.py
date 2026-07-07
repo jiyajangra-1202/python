003e5a24c59148a85cd6a01f5cee222511cf692d
jiyakumari-1202 <jiyajangra644@gmail.com>
2026-07-07 21:45:18 +0530

    loop.py

---
+++ loop.py
@@ -0,0 +1,11 @@
+#loop is used to execute a block of code repeatedly
+#Using a loop
+for i in range(5):
+    print("Hello World")
+#TYPES OF LOOPS IN PYTHON
+#Syntax of for loop(used to iterate over a sequences)
+# it used in string,list,tuple,set,dictionary or range
+for i in range(1,6):
+    print(i)    #print numbers
+    for i in range(5):
+        print("Jiya")#print a name 5 times
