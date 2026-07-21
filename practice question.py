4570de0f6142ebd953a46d6d786c2170632131d7
jiyakumari-1202 <jiyajangra644@gmail.com>
2026-07-21 21:14:31 +0530

    update practice questions.py

--- practice question.py
+++ practice question.py
@@ -1,7 +1,7 @@
 ##Student Result Management Program:
 students=[]
 #number of students:
-n=int(input("enter the number of students:"))
+n=(input("enter the number of students:"))
 for i in range(n):
     print(f"\nEnter details of student {i+1}")
 
@@ -52,3 +52,83 @@ for student in students:
     print("Average  :", round(student[4],2))
     print("Grade    :", student[5])
 
+###PYTHON PROJECT PRACTICE SET:-
+#PROJECT1:STUDENT REPORT CARD SYSTEM
+name=input("enter the name:")
+marks=[]
+for i in range(5):
+    mark=float(input("enter marks of subject {i+1}:"))
+    marks.append(mark)
+
+total=sum(marks)
+average=total/5
+percentage=average
+
+if percentage >=90:
+    grade="A+"
+if percentage >=-80:
+    grade="A"
+if percentage >=70:
+    grade="B"
+if percentage >=50:
+    grade="C"    
+else:
+    grade="Fail"
+
+print("\n---------Report Card----------")
+print("name:",name)        
+print("Marks:",marks)        
+print("Total:",total)        
+print("Average:",average)        
+print("Percentage:",percentage)  
+print("Grade:",grade)        
+
+#PROJECT2:ATM Machine
+balance=5000
+while True:
+    print("\n1.check balance")
+    print("2.Deposit")
+    print("3.Withdraw")
+    print("4.Exit")
+
+    choice=int(input("enter choice:"))
+    if choice==1:
+        print("current balance:",balance)
+    elif choice==2:
+        amount=float(input("enter amount to deposit:"))
+        balance +=amount
+        print("updated balance:",balance)
+    elif choice==3:
+        amount=float(input("enter amount to withdraw:"))
+        if amount <=balance:
+            balance -=amount
+            print("please collect cash.")
+        else:
+            print("insufficient balance")
+    elif choice==4:
+        print("Thank you!")
+        break
+    else:
+        print("invalid choice")
+
+#PROJECT3:PASSWORD STRENGTH CHECKER:-
+password=input("enter password:")
+upper=False
+lower=False
+digit=False
+special=False
+
+for ch in password:
+    if ch.isupper():
+        upper=True
+    elif ch.islower():
+        lower=True
+    elif ch.isdigit():
+        digit=True
+    else:
+        special=True
+
+if len(password) >=8 and upper and lower and digit and special:
+    print("Strong password")
+else:
+    print("weak password")    
